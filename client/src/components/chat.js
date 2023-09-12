a72ba0d5342641e2d5bdc0c744671293ae7a577b
cleverice007 <cleverice90128@gmail.com>
2023-09-12 14:45:06 +0800

    simple websocket message done

--- client/src/components/chat.js
+++ client/src/components/chat.js
@@ -1,20 +1,34 @@
-import React, { useEffect,useRef,useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUsername, setTargetUsername, addMessage } from '../store/userSlice';
 
 
 
 const Chat = () => {
-  const ws = useRef(null); 
+  const ws = useRef(null);
   const dispatch = useDispatch();
   const username = useSelector((state) => state.user.username);
   const targetUsername = useSelector((state) => state.user.targetUsername);
   const messages = useSelector((state) => state.user.messages);
   const [messageContent, setMessageContent] = useState('');
 
- 
+
   useEffect(() => {
     ws.current = new WebSocket('ws://localhost:3000');
+
+    ws.current.onopen = () => {
+      console.log("WebSocket connection opened");
+    };
+
+    ws.current.onerror = (event) => {
+      console.log("WebSocket Error:", event);
+      if (event instanceof ErrorEvent) {
+        console.log(`Error Message: ${event.message}`);
+      } else {
+        console.log("Not an ErrorEvent");
+      }
+    };
+    
     ws.current.addEventListener('message', (event) => {
       try {
         const newMessage = JSON.parse(event.data);
@@ -23,12 +37,12 @@ const Chat = () => {
         console.error('Failed to parse the received message as JSON:', event.data);
       }
     });
-  
+
     return () => {
       ws.current.close();
     };
   }, [dispatch]);
-  
+
 
   const sendMessage = () => {
     if (ws.current && ws.current.readyState === WebSocket.OPEN) {
@@ -54,9 +68,9 @@ const Chat = () => {
         placeholder="Target username"
         onChange={(e) => dispatch(setTargetUsername(e.target.value))}
       />
-      <textarea 
-        placeholder="Your message here" 
-        onChange={(e) => setMessageContent(e.target.value)} 
+      <textarea
+        placeholder="Your message here"
+        onChange={(e) => setMessageContent(e.target.value)}
       ></textarea>
       <button onClick={sendMessage}>Send</button>
       <textarea readOnly value={messages.map((msg) => `${msg.from}: ${msg.content}`).join('\n')} />
--- server/app.js
+++ server/app.js
@@ -2,31 +2,44 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+
 const cors = require('cors');
 
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 app.use(cors());
-
 let users = {};
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     let parsedMessage = JSON.parse(message);
 
+    // 如果是第一次連接，根據"from"字段設置用戶名
+    if (parsedMessage.from && !ws.username) {
+      ws.username = parsedMessage.from;
+      users[ws.username] = ws;
+    }
+
     switch (parsedMessage.type) {
-      case 'login':
-        users[parsedMessage.username] = ws;
-        ws.username = parsedMessage.username;
-        break;
       case 'message':
-        const targetWs = users[parsedMessage.target];
-        targetWs.send(JSON.stringify({ type: 'message', content: parsedMessage.content }));
+        const targetWs = users[parsedMessage.to];
+        if (targetWs && targetWs.readyState === WebSocket.OPEN) {
+          targetWs.send(
+            JSON.stringify({
+              type: 'message',
+              from: parsedMessage.from,
+              content: parsedMessage.content,
+            })
+          );
+        } else {
+          console.log("Target WebSocket is not available.");
+        }
         break;
     }
   });
 });
+
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
