f33b92ccb0226119c86a39fe5f4d391dbdedf7b8
cleverice007 <cleverice90128@gmail.com>
2023-09-16 11:01:41 +0800

    讓src內的檔案可以被tailwind 掃描

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx', // for React
+    './src/**/*.ts',  // for TypeScript
+    './src/**/*.tsx', // for TypeScript React
+    // etc.
+  ],  theme: {
     extend: {},
   },
   plugins: [],
