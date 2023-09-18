7651bc1d9322ee51c76a0974a095ca4c3a7fb0d0
cleverice007 <cleverice90128@gmail.com>
2023-09-18 22:32:15 +0800

    把form data 送入mySQL

--- server/controllers/userProfileController.js
+++ server/controllers/userProfileController.js
@@ -1,6 +1,24 @@
-module.exports.submitUserProfile = (req, res) => {
-    const userProfileData = req.body;
-    console.log("User Profile Data:", userProfileData);
-  
+const UserProfile = require('../models/UserProfile'); 
+
+module.exports.submitUserProfile = async (req, res) => {
+  const userProfileData = req.body;
+  console.log("User Profile Data:", userProfileData);
+
+  try {
+    const newUserProfile = await UserProfile.create({
+      profilePicture: userProfileData.profilePicture,
+      name: userProfileData.name,
+      age: userProfileData.age,
+      gender: userProfileData.gender,
+      aboutMe: userProfileData.aboutMe,
+      interests: userProfileData.interests,
+      location: userProfileData.location,
+    });
+
+    console.log('New User Profile Created:', newUserProfile);
     res.json({ message: "User profile submitted successfully!" });
-  };
+  } catch (error) {
+    console.log("Error Creating User Profile:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
