739b28c92b9db239a35aa73a8df4ead1d298f1fc
cleverice007 <cleverice90128@gmail.com>
2023-09-19 11:08:05 +0800

    使用aws sdk v3 not v2

--- server/controllers/userProfileController.js
+++ server/controllers/userProfileController.js
@@ -1,22 +1,20 @@
-const UserProfile = require('../models/UserProfile'); 
-const AWS = require('aws-sdk');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const UserProfile = require('../models/UserProfile');
 
 // 初始化 S3 服務
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
-
 
 module.exports.submitUserProfile = async (req, res) => {
   const userProfileData = req.body;
-
-  // 取得上傳的檔案
   const file = req.files.profilePicture;
   const fileName = `${Date.now()}_${file.name}`;
 
-  // S3 上傳參數
   const uploadParams = {
     Bucket: 'tinder-clone-mason',
     Key: fileName,
@@ -24,12 +22,11 @@ module.exports.submitUserProfile = async (req, res) => {
   };
 
   try {
-    // 上傳到 S3
-    const s3Data = await s3.upload(uploadParams).promise();
-
-    // 現在 s3Data.Location 包含了上傳圖片的 URL
+    const command = new PutObjectCommand(uploadParams);
+    await s3.send(command);
+    
     const newUserProfile = await UserProfile.create({
-      profilePicture: s3Data.Location,  // 將 S3 URL 儲存
+      profilePicture: `https://${uploadParams.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`,  // 將 S3 URL 儲存
       name: userProfileData.name,
       age: userProfileData.age,
       gender: userProfileData.gender,
@@ -37,8 +34,7 @@ module.exports.submitUserProfile = async (req, res) => {
       interests: userProfileData.interests,
       location: userProfileData.location
     });
-
-    console.log('New User Profile Created:', newUserProfile);
+  
     res.json({ message: "User profile submitted successfully!" });
   } catch (error) {
     console.log("Error Creating User Profile:", error);
