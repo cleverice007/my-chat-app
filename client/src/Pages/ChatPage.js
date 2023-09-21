e194c7c0d372b78a7d0c829c5c4809edf6823e17
cleverice007 <cleverice90128@gmail.com>
2023-09-21 16:04:05 +0800

    前端使用socket.io

--- client/src/Pages/ChatPage.js
+++ client/src/Pages/ChatPage.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import UserCard from '../components/UserCard';
 import ChatPanel from '../components/ChatPanel';
 import Navbar from '../components/Navbar';
-
+import io from 'socket.io-client';
 
 const ChatPage = () => {
     const [selectedUser, setSelectedUser] = useState(null);
+    const socketRef = useRef(null);
     const users = [
         { username: 'Alice', profilePicture: '/path/to/alice.jpg' },
         // ...
@@ -13,6 +14,21 @@ const ChatPage = () => {
     const messages = [
         // Fetch these from your state or server
     ];
+
+    useEffect(() => {
+      socketRef.current = io(process.env.REACT_APP_WS_URL || 'http://localhost:3000');
+
+      socketRef.current.on('connect', () => {
+        console.log('Socket.io connected');
+      });
+
+      // Add more socket event listeners as needed
+
+      return () => {
+        socketRef.current.disconnect();
+      };
+    }, []);
+
     return (
         <div className="chat-page flex flex-col">
           <div className="upper-section mb-10"> 
@@ -26,16 +42,16 @@ const ChatPage = () => {
                   username={user.username}
                   profilePicture={user.profilePicture}
                   onClick={() => setSelectedUser(user.username)}
+                  socket={socketRef.current}
                 />
               ))}
             </div>
             <div className="chat-section w-3/4">
-              {selectedUser && <ChatPanel messages={messages} />}
+              {selectedUser && <ChatPanel messages={messages} socket={socketRef.current} />}
             </div>
           </div>
         </div>
       );
-      
 };
 
 export default ChatPage;
