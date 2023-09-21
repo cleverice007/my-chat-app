811bf9f2cac2f0c1073386022dceee933b6abb16
cleverice007 <cleverice90128@gmail.com>
2023-09-21 18:33:17 +0800

    新增1:1聊天室會用到的database

---
+++ server/models/ChatRoom.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const ChatRoom = sequelize.define('ChatRoom', {
+  chatRoomId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user1Id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'UserProfile',
+      key: 'userId',
+    },
+  },
+  user2Id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'UserProfile', 
+      key: 'userId',
+    },
+  },
+  roomName: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+module.exports = ChatRoom;
---
+++ server/models/Message.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Message = sequelize.define('Message', {
+  messageId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  chatRoomId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'ChatRooms',
+      key: 'chatRoomId',
+    },
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'UserProfiles',
+      key: 'userId',
+    },
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+module.exports = Message;
