42a1cf5c186be6b6b815047dceb73832f176e658
cleverice007 <cleverice90128@gmail.com>
2023-09-25 21:50:32 +0800

    創建、搜尋聊天室、message 的後端function

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -3,76 +3,46 @@ const Message = require('../models/Message');
 const { Op } = require('sequelize');
 
 
-module.exports.createOrFetchChatRoom = async (req, res) => {
+module.exports.getOrCreateChatAndFetchMessages = async (req, res) => {
   try {
-    const { user1, user2 } = req.body;
-
-    // 首先嘗試找到是否已經有一個現存的聊天室
-    let chatRoom = await ChatRoom.findOne({
+    const { loggedInUserId, selectedUserId } = req.query;
+    
+    // 先檢查是否存在聊天室
+    let chatRoom = await ChatRoomModel.findOne({
       where: {
         [Op.or]: [
-          { user1Id: user1, user2Id: user2 },
-          { user1Id: user2, user2Id: user1 }
+          { user1Id: loggedInUserId, user2Id: selectedUserId },
+          { user1Id: selectedUserId, user2Id: loggedInUserId }
         ]
       }
     });
 
-    // 如果沒有找到，則創建一個新的聊天室
+    // 如果不存在，則創建一個
     if (!chatRoom) {
-      chatRoom = await ChatRoom.create({
-        user1Id: user1,
-        user2Id: user2
+      chatRoom = await ChatRoomModel.create({
+        user1Id: loggedInUserId,
+        user2Id: selectedUserId,
       });
     }
 
-    // 接著，找到該聊天室中的所有訊息
-    const messages = await Message.findAll({
+    // 獲取該聊天室內的訊息
+    const messages = await MessageModel.findAll({
       where: {
-        chatRoomId: chatRoom.chatRoomId
+        chatRoomId: chatRoom.chatRoomId,
       },
-      order: [['createdAt', 'ASC']]
     });
 
-    // 將聊天室和訊息返回給前端
+    // 返回結果
     res.status(200).json({
-      chatRoom: chatRoom,
-      messages: messages
+      chatRoom,
+      messages
     });
 
   } catch (error) {
-    console.error("Error fetching or creating chat room:", error);
-    res.status(500).send("Internal Server Error");
+    console.error("Error:", error);
+    res.status(500).json({
+      error: "Internal Server Error"
+    });
   }
 };
 
-module.exports.getChatMessages = async (req, res) => {
-  const { user1Id, user2Id } = req.query;
-
-  try {
-    // 尋找 chat room
-    const chatRoom = await ChatRoom.findOne({
-      where: {
-        [Op.or]: [
-          { user1Id: user1Id, user2Id: user2Id },
-          { user1Id: user2Id, user2Id: user1Id }
-        ]
-      }
-    });
-
-    if (!chatRoom) {
-      return res.status(404).json({ message: "Chat room not found" });
-    }
-
-    // 獲取該 chat room 的訊息
-    const messages = await Message.findAll({
-      where: { chatRoomId: chatRoom.chatRoomId },
-      order: [['createdAt', 'ASC']]
-    });
-
-    res.json(messages);
-
-  } catch (error) {
-    console.error('Error fetching chat messages:', error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const chatController = require("../controllers/chatController");
 
-router.post("/api/userprofiles",chatController.createOrFetchChatRoom);
-app.get('/api/chats', chatController.getChatMessages);
-
+router.get('api/getOrCreateChatAndFetchMessages ',chatController.getOrCreateChatAndFetchMessages);
 // 導出這個路由對象，以便在其他檔案中使用
 module.exports.router = router;
