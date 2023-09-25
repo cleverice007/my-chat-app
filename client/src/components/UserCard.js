a2331fcf9bcb3bd373e99f18f966c7092fa55029
cleverice007 <cleverice90128@gmail.com>
2023-09-25 11:20:51 +0800

    在usercard 獲得從redux 獲得的name

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { useSelector } from 'react-redux'; 
 
 const fetchOrCreateChatRoom = async (loggedInUsername, cardUsername) => {
   try {
@@ -13,34 +14,32 @@ const fetchOrCreateChatRoom = async (loggedInUsername, cardUsername) => {
     console.log("Chat room info:", chatRoom);
     console.log("Messages:", messages);
     
-    // 這裡可以進一步處理，比如將 chatRoom 和 messages 送到 Redux store 或某個 state 管理系統。
     
   } catch (error) {
     console.error("Error fetching or creating chat room:", error);
   }
 };
 
-const UserCard = ({ username, profilePicture }) => (
-  <div 
-    className="user-card bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer transform hover:scale-105 transition-transform duration-300 ease-in-out" 
-    onClick={() => {
-      // 從 URL 中獲取當前登入用戶的 username。
-      const loggedInUsername = window.location.pathname.split("/chat/")[1];
-      
-      // 觸發上面的 async 函數。
-      fetchOrCreateChatRoom(loggedInUsername, username);
-    }}
-  >
-    <img 
-      className="w-20 h-20 object-cover" 
-      src={profilePicture} 
-      alt={`${username}'s profile`} 
-    />
-    <div className="p-4">
-      <h3 className="font-bold text-lg">{username}</h3>
+const UserCard = ({ username, profilePicture }) => {
+  const loggedInUsername = useSelector(state => state.user.name); // 使用 useSelector 從 Redux 獲取 name
+  return (
+    <div 
+      className="user-card bg-white rounded-lg shadow-lg overflow-hidden cursor-pointer transform hover:scale-105 transition-transform duration-300 ease-in-out" 
+      onClick={() => {
+        fetchOrCreateChatRoom(loggedInUsername, username); // 使用從 Redux 獲得的 loggedInUsername
+      }}
+    >
+      <img 
+        className="w-20 h-20 object-cover" 
+        src={profilePicture} 
+        alt={`${username}'s profile`} 
+      />
+      <div className="p-4">
+        <h3 className="font-bold text-lg">{username}</h3>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default UserCard;
 
