48eb6cc1846c1c639751f75baea08131be807668
cleverice007 <cleverice90128@gmail.com>
2023-09-27 15:35:12 +0800

    chatroute debug

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const chatController = require("../controllers/chatController");
 
-router.get('api/getOrCreateChatAndFetchMessages ',chatController.getOrCreateChatAndFetchMessages);
+router.get('/api/getOrCreateChatAndFetchMessages', chatController.getOrCreateChatAndFetchMessages);
 // 導出這個路由對象，以便在其他檔案中使用
 
 module.exports = router;
