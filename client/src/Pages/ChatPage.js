6698109cfab1f3f19766c2effd21c1988a36872d
cleverice007 <cleverice90128@gmail.com>
2023-09-28 17:57:11 +0800

    前端發送、接收訊息的function （socket.io

--- client/src/Pages/ChatPage.js
+++ client/src/Pages/ChatPage.js
@@ -6,87 +6,74 @@ import Navbar from '../components/Navbar';
 import io from 'socket.io-client';
 import { useSelector } from 'react-redux';
 
-
 const ChatPage = () => {
   const [selectedUserId, setSelectedUserId] = useState(null);
   const socketRef = useRef(null);
   const [users, setUsers] = useState([]);
   const [messages, setMessages] = useState([]);
-
+  
+  // 從 Redux 獲取當前登入用戶的 ID
   const loggedInUserId = useSelector(state => state.user.userId);
-  console.log("Outside useEffect, loggedInUserId:", loggedInUserId); 
-
-
+  
+  // 異步方法來創建或獲取聊天室和消息
   const fetchOrCreateChatAndFetchMessages = async (loggedInUserId, selectedUserId) => {
     try {
       const response = await axios.get(`/api/getOrCreateChatAndFetchMessages?loggedInUserId=${loggedInUserId}&selectedUserId=${selectedUserId}`);
       const { chatRoom, messages } = response.data;
-
-      console.log("Chat room info:", chatRoom);
-      console.log("Messages:", messages);
-
+      
       // 更新 messages 狀態
       setMessages(messages);
-
     } catch (error) {
       console.error("Error fetching or creating chat room and messages:", error);
     }
   };
-
+  
+  // 使用 useEffect 進行初始化
   useEffect(() => {
     if (loggedInUserId) {
-      console.log("Current loggedInUserId:", loggedInUserId);  // 追踪 loggedInUserId
-
+      console.log("Current loggedInUserId:", loggedInUserId);
+      
+      // 建立 socket 連接
       socketRef.current = io(process.env.REACT_APP_WS_URL);
-
+      
+      // 監聽 connect 事件
       socketRef.current.on('connect', () => {
         console.log('Socket.io connected');
+        
+        // 發送 init 事件，並將 loggedInUserId 作為 username
+        socketRef.current.emit('init', loggedInUserId);
       });
-
-      socketRef.current.on('privateMessage', (message) => {
-        console.log('Received private message:', message);
-      });
-
+      
+      // 從後端獲取所有用戶資料
       axios.get('/api/userprofiles')
         .then(response => {
-          const filteredUsers = response.data.filter(user => user.userId !== loggedInUserId); // 過濾掉當前登入用戶
+          const filteredUsers = response.data.filter(user => user.userId !== loggedInUserId);
           setUsers(filteredUsers);
         })
         .catch(error => {
           console.error('Error fetching user profiles:', error);
         });
     }
-
+    
+    // 清理操作
     return () => {
       if (socketRef.current) {
         socketRef.current.disconnect();
       }
     };
-  }, [loggedInUserId]);  // 注意這裡添加了 loggedInUserId 作為依賴
-
+  }, [loggedInUserId]); // 依賴於 loggedInUserId
 
   return (
-    <div className="chat-page flex flex-col">
-      <div className="upper-section mb-10">
-        <Navbar />
-      </div>
-      <div className="lower-section flex">
-        <div className="user-list w-1/4">
-          {users.map((user, index) => (
-            <UserCard
-            key={index}
-            userId={user.userId}
-            username={user.name}
-            profilePicture={user.profilePicture}
-            onClick={() => {
-              fetchOrCreateChatAndFetchMessages(loggedInUserId, user.userId);
-              setSelectedUserId(user.userId);
-            }}
-          />
+    <div>
+      <Navbar />
+      <div className="chat-page">
+        <div className="user-list">
+          {users.map(user => (
+            <UserCard key={user.userId} user={user} onClick={() => fetchOrCreateChatAndFetchMessages(loggedInUserId, user.userId)} />
           ))}
         </div>
-        <div className="chat-section w-3/4">
-          {selectedUserId && <ChatPanel messages={messages} socket={socketRef.current} />}
+        <div className="chat-panel">
+          {selectedUserId && <ChatPanel messages={messages} socket={socketRef.current} loggedInUserId={loggedInUserId} selectedUserId={selectedUserId} />}
         </div>
       </div>
     </div>
--- client/src/components/ChatPanel.js
+++ client/src/components/ChatPanel.js
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; // 新增 useState
 import Message from './Message';
 
-const ChatPanel = ({ messages, socket }) => {
+const ChatPanel = ({ messages, socket, loggedInUserId, selectedUserId }) => {
+  const [localMessages, setLocalMessages] = useState(messages);
+  const [messageInput, setMessageInput] = useState(''); // 新增 state 管理 textarea
+
   const sendMessage = (content) => {
-    socket.emit('privateMessage', {
-      to: 'someTargetUsername',  // 將這個值替換成實際的目標使用者名稱
+    const newMessage = {
+      from: loggedInUserId,
+      to: selectedUserId,
       message: content,
+    };
+    socket?.emit('privateMessage', newMessage);
+    setLocalMessages([...localMessages, newMessage]);
+  };
+
+  const receiveMessage = () => {
+    socket?.on('privateMessage', (message) => {
+      console.log("接收到的私人訊息:", message);
+      setLocalMessages(prevMessages => [...prevMessages, message]);
     });
   };
 
+  const handleTextareaChange = (e) => {
+    setMessageInput(e.target.value);
+  };
+
+  useEffect(() => {
+    setLocalMessages(messages);
+
+    if (!socket) {
+      console.warn("Socket 未初始化");
+      return;
+    }
+
+    // Receive messages
+    receiveMessage();
+
+    // Cleanup
+    return () => {
+      socket.off('privateMessage');
+    };
+  }, [socket, messages]);
+
   return (
     <div className="chat-panel">
-      {messages.map((msg, index) => (
-        <Message key={index} content={msg.content} from={msg.from} />
-      ))}
-      <button onClick={() => sendMessage('Hello, world!')}>Send Message</button>
+      <div className="messages-section overflow-auto h-64 bg-gray-100 p-4">
+        {localMessages.map((msg, index) => (
+          <Message key={index} content={msg.message} from={msg.from} />
+        ))}
+      </div>
+      <div className="input-section bg-gray-200 p-4 flex items-center">
+        <textarea
+          value={messageInput}
+          onChange={handleTextareaChange}
+          className="flex-1 p-2 border rounded"
+          placeholder="Type a message..."
+        ></textarea>
+        <button
+          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={() => sendMessage(messageInput)}
+        >
+          Send Message
+        </button>
+      </div>
     </div>
   );
 };
+
+
 
 export default ChatPanel;
 
