3247c59a8b211419ca1ae22d62be1d730915a236
cleverice007 <cleverice90128@gmail.com>
2023-10-01 10:29:24 +0800

    debug

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userAuthController = require('../controllers/userAuthController'); 
 
 // 處理註冊路由
-router.post('/register', userAuthController.registerUser);
+router.post('/api/register', userAuthController.registerUser);
 
 // 處理登錄路由
-router.post('/login', userAuthController.loginUser);
+router.post('/api/login', userAuthController.loginUser);
 
 module.exports = router;
