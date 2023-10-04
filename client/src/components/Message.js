1c0ce61e8c81c790a784f80d77fd948abe690603
cleverice007 <cleverice90128@gmail.com>
2023-10-04 18:47:56 +0800

    左右對調

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -18,9 +18,9 @@ const Message = ({ content, from, createdAt, loggedInUser, selectedUser }) => {
   const profilePicture = isCurrentUser ? loggedInUser.profilePicture : selectedUser.profilePicture;
 
   return (
-    <div className={`flex items-center ${isCurrentUser ? 'justify-end' : 'justify-start'} mb-2`}>
+    <div className={`flex items-center ${isCurrentUser ? 'justify-start' : 'justify-end'} mb-2`}>
       <img src={profilePicture} alt="Profile" className="w-10 h-10 rounded-full" />
-      <div className={`ml-2 rounded-lg p-2 ${isCurrentUser ? 'bg-green-200' : 'bg-gray-200'}`}>
+      <div className={`ml-2 rounded-lg p-2 ${isCurrentUser ? 'bg-gray-200' : 'bg-green-200'}`}>
         <strong>{from}</strong>: {content} <span className="text-sm text-gray-600">({formatDate(createdAt)})</span>
       </div>
     </div>
