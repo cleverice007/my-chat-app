a666e334feb004ef492845f9ae8d3b0b9d6306ef
cleverice007 <cleverice90128@gmail.com>
2023-10-12 12:07:26 +0800

    建立usermatching 和其他schema的關聯

--- server/models/index.js
+++ server/models/index.js
@@ -5,6 +5,7 @@ const UserProfile = require('./UserProfile');
 const ChatRoom = require('./ChatRoom');
 const Message = require('./Message');
 const UserAuth = require('./UserAuth');  
+const UserMatching = require('./UserMatching'); 
 
 // 定義關聯
 ChatRoom.hasMany(Message, { foreignKey: 'chatRoomId' });
@@ -17,9 +18,17 @@ UserProfile.hasMany(Message, { foreignKey: 'userId' });
 UserAuth.hasOne(UserProfile, { foreignKey: 'userId' });
 UserProfile.belongsTo(UserAuth, { foreignKey: 'userId' });
 
+// 建立 UserAuth 和 UserMatching 之間的一對多關聯
+UserAuth.hasMany(UserMatching, { foreignKey: 'user1Id' });
+UserAuth.hasMany(UserMatching, { foreignKey: 'user2Id' });
+UserMatching.belongsTo(UserAuth, { as: 'User1', foreignKey: 'user1Id' });
+UserMatching.belongsTo(UserAuth, { as: 'User2', foreignKey: 'user2Id' });
+
+
 // 匯出所有模型
 module.exports = {
   UserProfile,
   ChatRoom,
   Message,
-  UserAuth};
+  UserAuth,
+  UserMatching,};
