b4bd473f58a1ff65d9088c6f383eeb5c0fca04ff
cleverice007 <cleverice90128@gmail.com>
2023-10-14 23:07:54 +0800

    debug 嘗試顯示即時訊息

--- client/src/components/ChatPanel.js
+++ client/src/components/ChatPanel.js
@@ -11,7 +11,7 @@ const ChatPanel = ({ messages, socket, loggedInUser, selectedUser }) => {
     const newMessage = {
       from: loggedInUserId,
       to: selectedUser.userId,
-      message: content,
+      content: content,
       createdAt: timestamp, 
         };
     socket?.emit('privateMessage', newMessage);
