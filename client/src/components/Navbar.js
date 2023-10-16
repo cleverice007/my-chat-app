58d5e9d506e2a811b7609c265780b0f0c2ee7633
cleverice007 <cleverice90128@gmail.com>
2023-10-16 12:58:18 +0800

    在navbar 增加link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className="navbar bg-gray-600 text-white p-4">
-            <span className="mx-4 font-bold text-2xl cursor-pointer hover:text-gray-300">Tinder</span>
-            <span className="mx-4 cursor-pointer hover:text-gray-300">聊天</span>
-            <span className="mx-4 cursor-pointer hover:text-gray-300">個人檔案</span>
+            <span className="mx-4 font-bold text-2xl cursor-pointer hover:text-gray-300">
+                Tinder
+            </span>
+            <Link to="/chat" className="mx-4 cursor-pointer hover:text-gray-300">
+                聊天
+            </Link>
+            <Link to="/userprofile" className="mx-4 cursor-pointer hover:text-gray-300">
+                個人檔案
+            </Link>
+            <Link to="/match" className="mx-4 cursor-pointer hover:text-gray-300">
+                配對
+            </Link>
         </div>
     );
 };
+
 
 export default Navbar;
